d53595246883afd92ce9020b6d86fe4aff1f7dd6
pixel-Teee <47231777+pixel-Teee@users.noreply.github.com>
2026-04-16 21:24:24 +0800

    implement anim layer

--- Source/Warrior/Private/Components/Combat/HeroCombatComponent.cpp
+++ Source/Warrior/Private/Components/Combat/HeroCombatComponent.cpp
@@ -2,7 +2,10 @@
 
 
 #include "Components/Combat/HeroCombatComponent.h"
+#include "Items/Weapons/WarriorHeroWeapon.h"
 
-
-
+AWarriorHeroWeapon* UHeroCombatComponent::GetHeroCarriedWeaponByTag(FGameplayTag InWeaponTag)
+{
+	return Cast<AWarriorHeroWeapon>(GetCharacterCarriedWeaponByTag(InWeaponTag));
+}
 
--- Source/Warrior/Public/Components/Combat/HeroCombatComponent.h
+++ Source/Warrior/Public/Components/Combat/HeroCombatComponent.h
@@ -6,6 +6,7 @@
 #include "Components/Combat/PawnCombatComponent.h"
 #include "HeroCombatComponent.generated.h"
 
+class AWarriorHeroWeapon;
 /**
  * 
  */
@@ -14,7 +15,9 @@ class WARRIOR_API UHeroCombatComponent : public UPawnCombatComponent
 {
 	GENERATED_BODY()
 	
-	
+public:
+	UFUNCTION(BlueprintCallable, Category = "Warrior|Combat")
+	AWarriorHeroWeapon* GetHeroCarriedWeaponByTag(FGameplayTag InWeaponTag);
 	
 	
 };
