3daa811fdf1a179586af39395649e07deb8b73f4
pixel-Teee <47231777+pixel-Teee@users.noreply.github.com>
2026-06-21 18:03:46 +0800

    update motion wrap target

--- Source/Warrior/Private/Characters/WarriorBaseCharacter.cpp
+++ Source/Warrior/Private/Characters/WarriorBaseCharacter.cpp
@@ -4,7 +4,7 @@
 #include "Characters/WarriorBaseCharacter.h"
 #include "AbilitySystem/WarriorAbilitySystemComponent.h"
 #include "AbilitySystem/WarriorAttributeSet.h"
-
+#include "MotionWarpingComponent.h"
 // Sets default values
 AWarriorBaseCharacter::AWarriorBaseCharacter()
 {
@@ -15,7 +15,7 @@ AWarriorBaseCharacter::AWarriorBaseCharacter()
 	GetMesh()->bReceivesDecals = false;
 
 	WarriorAbilitySystemComponent = CreateDefaultSubobject<UWarriorAbilitySystemComponent>(TEXT("WarriorAbilitySystemComponent"));
-
+	MotionWarpingComponent = CreateDefaultSubobject<UMotionWarpingComponent>(TEXT("MotionWarpingComponent"));
 	WarriorAttributeSet = CreateDefaultSubobject<UWarriorAttributeSet>(TEXT("WarriorAttributeSet"));
 }
 
