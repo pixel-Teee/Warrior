e8166ab791b540d0fb643e9f3a2aeb56521f370b
pixel-Teee <47231777+pixel-Teee@users.noreply.github.com>
2026-06-21 17:20:58 +0800

    fix damage apply to enemy

--- Source/Warrior/Public/AbilitySystem/Abilities/WarriorEnemyGameplayAbility.h
+++ Source/Warrior/Public/AbilitySystem/Abilities/WarriorEnemyGameplayAbility.h
@@ -23,6 +23,8 @@ public:
 	UFUNCTION(BlueprintPure, Category = "Warrior|Ability")
 	UEnemyCombatComponent* GetEnemyCombatComponentFromActorInfo();
 
+	UFUNCTION(BlueprintPure, Category = "Warrior|Ability")
+	FGameplayEffectSpecHandle MakeEnemyDamageEffectSpecHandle(TSubclassOf<UGameplayEffect> EffectClass, const FScalableFloat& InDamageScalableFloat);
 private:
 	TWeakObjectPtr<AWarriorEnemyCharacter> CachedWarriorEnemyCharacter;
 };
